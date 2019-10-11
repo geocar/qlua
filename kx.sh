09caa7be08a3168553d17d2f9bf9f02467e85fac
Rian Ó Cuinneagáin <rocuinneagain@gmail.com>
2019-10-11 15:03:21 +0100

    Pull kx files from github

--- kx.sh
+++ kx.sh
@@ -1,6 +1,7 @@
-mkdir -p kx
-curl -o kx/k.h http://kx.com/q/c/c/k.h
-curl -o kx/l32/c.o http://kx.com/q/l32/c.o
-curl -o kx/l64/c.o http://kx.com/q/l64/c.o
-curl -o kx/m32/c.o http://kx.com/q/m32/c.o
-curl -o kx/m64/c.o http://kx.com/q/m64/c.o
+mkdir -p kx/l64 kx/l32 kx/m32 kx/m64
+BASEURL="https://raw.githubusercontent.com/KxSystems/kdb/master"
+curl -o kx/k.h "$BASEURL/c/c/k.h"
+curl -o kx/l32/c.o "$BASEURL/l32/c.o"
+curl -o kx/l64/c.o "$BASEURL/l64/c.o"
+curl -o kx/m32/c.o "$BASEURL/m32/c.o"
+curl -o kx/m64/c.o "$BASEURL/m64/c.o"
